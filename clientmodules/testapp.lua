8c6f967c861170e1e567d53458d7db9eb7179daf
AstroSnail <astrosnail@protonmail.com>
2023-11-22 15:17:39 +0000

    Add button to create new counters

--- clientmodules/testapp.lua
+++ clientmodules/testapp.lua
@@ -6,17 +6,27 @@ local elem = deps.elem
 ---@module "clientmodules.counter"
 local counter = deps.counter
 
-local a = counter(0)
-
-local b = counter(7)
+local all_counters = {
+	counter(0),
+	counter(7),
+}
+local n_counters = 2
 
 return function()
-    return elem.div {
+    local desc = {
+        elem.button {
+            onClick = function() all_counters[n_counters + 1] = counter(0) n_counters = n_counters + 1 end,
+            "+"
+        },
         elem.p {
             "Counters: "
         },
-        "A: ", a(),
-        "B: ", b(),
     }
+    for i = 1, n_counters do
+	    local j = i * 2 + 1
+	    desc[j] = string.char(64 + i) .. ":"
+	    desc[j+1] = all_counters[i]()
+    end
+    return elem.div(desc)
 
 end
